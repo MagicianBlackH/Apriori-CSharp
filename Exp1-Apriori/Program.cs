f897e45ae7674775ba374b6b62b685d01c6f9485
unknown <295481044@qq.com>
2022-10-07 21:43:14 +0800

    first commit

---
+++ Exp1-Apriori/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exp1_Apriori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
