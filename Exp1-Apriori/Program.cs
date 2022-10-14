5986675ad518af448e57c07a762c8bfbac6bcbc5
MagicianH Black <295481044@qq.com>
2022-10-14 12:55:36 +0800

    更改测试案例置信度

--- Exp1-Apriori/Program.cs
+++ Exp1-Apriori/Program.cs
@@ -12,7 +12,7 @@ namespace Exp1_Apriori
             // Main函数主要写使用逻辑，Apriori算法实现及置信度检验等写于Apriori.cs文件
             string[] data = File.ReadAllLines(@"..\..\..\example.txt", Encoding.UTF8);
             List<string> dataList = new List<string>(data);
-            Apriori apriori_test = new Apriori(0.6, 0, dataList);
+            Apriori apriori_test = new Apriori(0.6, 0.8, dataList);
             apriori_test.ShowOriginData();
             Console.WriteLine();
             apriori_test.ShowIteration();
