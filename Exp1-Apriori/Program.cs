b8a2a3d85d8a725f370bfe2e67f73d84050cf00e
MagicianH Black <295481044@qq.com>
2022-10-17 15:53:23 +0800

    修改使用数据

--- Exp1-Apriori/Program.cs
+++ Exp1-Apriori/Program.cs
@@ -10,9 +10,13 @@ namespace Exp1_Apriori
         static void Main(string[] args)
         {
             // Main函数主要写使用逻辑，Apriori算法实现及置信度检验等写于Apriori.cs文件
-            string[] data = File.ReadAllLines(@"..\..\..\example.txt", Encoding.UTF8);
+            string[] data = File.ReadAllLines(@"..\..\..\leetcode_tags.txt", Encoding.UTF8);
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = data[i].Replace("\"", "");
+            }
             List<string> dataList = new List<string>(data);
-            Apriori apriori_test = new Apriori(0.6, 0.8, dataList);
+            Apriori apriori_test = new Apriori(0.2, 0.65, dataList);
             apriori_test.ShowOriginData();
             Console.WriteLine();
             apriori_test.ShowIteration();
